4191f7fca8ee3dcd34ad685815e0036afd42f7d4
petriwna <petriwna43@gmail.com>
2025-05-20 14:41:42 +0300

    fix

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,13 +1,8 @@
-// import Splide from '@splidejs/splide';
-
 export class Main {
     constructor() {
         this.mobileMenu = document.querySelector('.mobile-menu');
         this.menuBtnOpen = document.querySelector('.menu-btn-open');
         this.menuBtnClose = document.querySelector('.menu-btn-close');
-        // this.splideContainer = document.querySelector('.career-section__splide');
-        //
-        // this.createSplide();
         this.addEventListeners();
     }
 
@@ -29,40 +24,4 @@ export class Main {
     disableScroll() {
         document.body.classList.toggle('is-scroll-disable');
     }
-
-    handleFormSubmit(event) {
-        event.preventDefault();
-
-        window.location.href = 'thank-you.html';
-    }
-
-    // createSplide(){
-    //     const isMobile = window.innerWidth < 1280;
-    //     if (isMobile) {
-    //         this.splideContainer.classList.add('splide');
-    //
-    //         this.initSplide();
-    //     }
-    // }
-    //
-    // initSplide() {
-    //     const splide = new Splide('.splide', {
-    //         type: 'loop',
-    //         gap: '10px',
-    //         drag: 'free',
-    //         pagination: true,
-    //         arrows: false,
-    //         perPage: 3,
-    //         breakpoints: {
-    //             768: {
-    //                 perPage: 2,
-    //             },
-    //             600: {
-    //                 perPage: 1,
-    //             }
-    //         },
-    //     });
-    //
-    //     splide.mount();
-    // }
 }
